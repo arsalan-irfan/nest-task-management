c6d66f5b5ced9054dc62d1ddac18e84129012461
Muhammad Arsalan <43111371+arsalan318@users.noreply.github.com>
2022-01-13 17:55:58 +0500

    User authentication added

---
+++ src/auth/auth.module.ts
@@ -0,0 +1,23 @@
+import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AuthController } from './auth.controller';
+import { AuthService } from './auth.service';
+import { JwtStrategy } from './jwt.strategy';
+import { UsersRepository } from './users.repository';
+
+@Module({
+  imports: [
+    PassportModule.register({ defaultStrategy: 'jwt' }),
+    JwtModule.register({
+      secret: 'topSecret51',
+      signOptions: { expiresIn: 3600 },
+    }),
+    TypeOrmModule.forFeature([UsersRepository]),
+  ],
+  controllers: [AuthController],
+  providers: [AuthService,JwtStrategy],
+  exports:[JwtStrategy,PassportModule]
+})
+export class AuthModule {}
